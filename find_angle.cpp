9e3f8ca0d3d51e3180f7cc005a4c07da0c39608c
Darshak Ranpariya <34344691+darshakpranpariya@users.noreply.github.com>
2020-07-14 18:18:46 +0530

    Create find_angle.cpp

---
+++ find_angle.cpp
@@ -0,0 +1,11 @@
+//program to calculate angle between hours and minutes...
+
+class Solution {
+public:
+    double angleClock(int hour, int minutes) {
+        double hour_angle = (hour % 12 ) * 30 + minutes * 0.5;
+        double minute_angle = minutes * 6;
+        double diff = abs(hour_angle - minute_angle);
+        return min(diff, 360 - diff);
+    }
+};
