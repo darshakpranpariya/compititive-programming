87d60920a308d2b13333ce783cbc5e44ca5fdf7a
Darshak Ranpariya <34344691+darshakpranpariya@users.noreply.github.com>
2020-07-22 17:49:24 +0530

    Create bfs+queue+pair.cpp

---
+++ bfs+queue+pair.cpp
@@ -0,0 +1,36 @@
+// Applied BFS algorithm......
+
+class Solution {
+public:
+    vector<int> ans[1000];
+    queue<pair<TreeNode*,int>> q;
+    void search(TreeNode* root,int level){
+        while(!q.empty()){
+            pair<TreeNode*,int> p_temp = q.front();
+            TreeNode* temp = p_temp.first;
+            ans[p_temp.second].push_back(temp->val);
+            level=p_temp.second+1;
+            if(temp->left!=NULL){
+                q.push(make_pair(temp->left,level));
+            }
+            if(temp->right!=NULL){
+                q.push(make_pair(temp->right,level));
+            }
+            q.pop();
+        }
+    }
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int>> ans_final;
+        if(root==NULL)
+            return ans_final;
+        int level=0;
+        q.push(make_pair(root,level));
+        search(root,level);
+        for(auto i:ans){
+            if(i.size()!=0)
+                ans_final.push_back(i);
+        }
+        reverse(ans_final.begin(),ans_final.end());
+        return ans_final;
+    }
+};
